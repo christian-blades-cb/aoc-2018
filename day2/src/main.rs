57da6d40d34dde583eb57271acea50694a702425
Christian Blades <cblades@pindrop.com>
2018-12-01 14:47:06 -0500

    scaffolded day2

---
+++ day2/src/main.rs
@@ -0,0 +1,30 @@
+#[macro_use]
+extern crate nom;
+extern crate regex;
+extern crate tap;
+
+use nom::{alpha, digit};
+use regex::Regex;
+use std::fs::File;
+use std::io::prelude::*;
+use tap::TapOps;
+
+fn main() -> Result<(), std::io::Error> {
+    let mut file = File::open("input-day2")?;
+    // let mut buf = Vec::new();
+    // file.read_to_end(&mut buf)?;
+    let mut buf = String::new();
+    file.read_to_string(&mut buf);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_hello() {
+        assert!(true);
+    }
+}
