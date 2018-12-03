2918ffd88b987b10909830cc47cd18987b0d01e3
Christian Blades <cblades@pindrop.com>
2018-12-03 10:53:05 -0500

    day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,15 +1,11 @@
 #[macro_use]
 extern crate nom;
-extern crate regex;
-extern crate tap;
 
+use nom::digit;
 use nom::types::CompleteStr;
-use nom::{alpha, digit, space};
-use regex::Regex;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::prelude::*;
-use tap::TapOps;
 
 fn main() -> Result<(), std::io::Error> {
     let mut file = File::open("input-day3")?;
@@ -25,9 +21,8 @@ fn main() -> Result<(), std::io::Error> {
         .collect();
 
     println!("day3.1 {}", part1(&boxes));
-    // Max(0, Min(XA2, XB2) - Max(XA1, XB1)) * Max(0, Min(YA2, YB2) - Max(YA1, YB1))
-
     println!("day3.2 {}", part2(&boxes));
+
     Ok(())
 }
 
@@ -49,29 +44,6 @@ fn part1(boxes: &[Rect]) -> usize {
     grid.iter().map(|x| if *x > 1 { 1 } else { 0 }).sum()
 }
 
-fn collision_dumb(l: &Rect, r: &Rect) -> bool {
-    use std::cmp::{max, min};
-    let mut grid: HashSet<usize> = HashSet::new();
-
-    let this = l;
-    for x in this.x..this.x + this.width {
-        for y in this.y..this.y + this.height {
-            grid.insert(coord_1d(x, y));
-        }
-    }
-    let this = r;
-    for x in this.x..this.x + this.width {
-        for y in this.y..this.y + this.height {
-            let coord = coord_1d(x, y);
-            if grid.contains(&coord) {
-                return true;
-            }
-        }
-    }
-
-    false
-}
-
 fn part2(boxes: &[Rect]) -> usize {
     let all_ids: HashSet<usize> = boxes.iter().map(|b| b.id).collect();
     let mut collided: HashSet<usize> = HashSet::new();
@@ -89,26 +61,6 @@ fn part2(boxes: &[Rect]) -> usize {
 
     let mut uncollided = all_ids.difference(&collided);
     uncollided.next().map(|x| *x).unwrap()
-}
-
-fn area_of_intersection(l: &Rect, r: &Rect) -> usize {
-    use std::cmp::{max, min};
-    max(0, min(l.x + l.width, r.x + r.width) - max(l.x, r.x))
-        * max(0, min(l.y + l.height, r.y + r.height) - max(l.y, r.y))
-}
-
-fn collision(rect1: &Rect, rect2: &Rect) -> bool {
-    // rect1.x < rect2.x + rect2.width
-    //     && rect1.x + rect1.width > rect2.x
-    //     && rect1.y < rect2.y + rect2.height
-    //     && rect1.y + rect1.height > rect2.y
-
-    // rect1.x < rect2.Right && RectA.Right > RectB.Left &&
-    //  RectA.Top > RectB.Bottom && RectA.Bottom < RectB.Top
-    rect1.x < rect2.x + rect2.width
-        && rect1.x + rect1.width > rect2.x
-        && rect1.y > rect2.y + rect2.height
-        && rect1.y + rect1.height < rect2.y
 }
 
 named!(parse_box<CompleteStr, Rect>,
@@ -147,7 +99,27 @@ struct Rect {
 mod test {
     use super::*;
 
-    fn test_hello() {
-        assert!(true);
+    fn get_boxes() -> Vec<Rect> {
+        let buf = include_str!("../../input-day3");
+        let boxes: Vec<Rect> = buf
+            .lines()
+            .map(|l| {
+                let (_, bo) = parse_box(l.into()).unwrap();
+                bo
+            })
+            .collect();
+        boxes
+    }
+
+    #[test]
+    fn test_part1() {
+        let boxes = get_boxes();
+        assert_eq!(104439, part1(&boxes));
+    }
+
+    #[test]
+    fn test_part2() {
+        let boxes = get_boxes();
+        assert_eq!(701, part2(&boxes));
     }
 }
